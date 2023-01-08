2adf9e8ba79d7bf96bac961cd60759205fb9aaf8
serega-stis <120461285+serega-stis@users.noreply.github.com>
2023-01-08 19:15:49 +0500

    test2

--- 331/currencies_stats.py
+++ 331/currencies_stats.py
@@ -8,15 +8,12 @@ currencies = {}
 for vacancy in data:
     year = vacancy.published_at.year
     currency = vacancy.salary.salary_currency
-    
     if not currency:
         continue
-
     if year not in currencies.keys():
         currencies[year] = {}
     if currency not in currencies[year].keys():
         currencies[year][currency] = 0
-    
     currencies[year][currency] += 1
 
 print(json.dumps(currencies, indent=4, ensure_ascii=False))
